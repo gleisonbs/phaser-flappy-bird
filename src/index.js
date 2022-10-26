39b9b71ca94aedd865c89eb8a51446c121077ddc
Gleison Batista <gleisonbs@gmail.com>
2022-10-25 22:31:17 -0300

    feat: add moving logic to bird

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      // gravity: { y: 200 }
+    }
   },
   scene: {
     preload,
     create,
+    update,
   }
 }
 
@@ -18,10 +22,26 @@ function preload() {
   this.load.image('bird', 'assets/bird.png')
 }
 
+let bird = null;
+const VELOCITY = 200;
+
 function create() {
   const { width, height } = config;
+  this.physics.world.setFPS(15);
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  this.add.sprite(width / 10, height/2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(width / 10, height/2, 'bird').setOrigin(0);
+  bird.body.velocity.x = VELOCITY;
 }
 
-new Phaser.Game(config);
+
+
+function update() {
+  const { width } = config;
+  if (bird.x + bird.width >= width) {
+    bird.body.velocity.x = -VELOCITY;
+  } else if (bird.x <= 0) {
+    bird.body.velocity.x = VELOCITY;
+  }
+}
+
+new Phaser.Game(config); 
